29412ffd84ff85a1b65efc96372054455ea530b5
JorgeDiezGracia <diezgraciajorge@gmail.com>
2026-05-23 21:29:41 +0200

    add Player class with jump

--- core/src/main/java/com/svalero/extremedownhill/screens/GameScreen.java
+++ core/src/main/java/com/svalero/extremedownhill/screens/GameScreen.java
@@ -4,22 +4,37 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.svalero.extremedownhill.ExtremeDownhill;
+import com.svalero.extremedownhill.entities.Player;
 
 public class GameScreen implements Screen {
 
     private final ExtremeDownhill game;
     private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
+    private Player player;
 
     public GameScreen(ExtremeDownhill game) {
         this.game = game;
         this.batch = new SpriteBatch();
+        this.shapeRenderer = new ShapeRenderer();
+        this.player = new Player(100, 100);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.2f, 0.6f, 0.2f, 1); // fondo verde bosque
+        // Limpiar pantalla
+        Gdx.gl.glClearColor(0.2f, 0.6f, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Actualizar jugador
+        player.update(delta);
+
+        // Dibujar jugador
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        player.render(shapeRenderer);
+        shapeRenderer.end();
     }
 
     @Override public void show() {}
@@ -31,5 +46,6 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         batch.dispose();
+        shapeRenderer.dispose();
     }
 }
